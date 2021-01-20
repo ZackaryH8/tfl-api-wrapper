90e4a54c10068f7993ef7e81e32a18d585e6c56b
ZackaryH8 <zackary12389@yahoo.co.uk>
2021-01-20 19:11:51 +0000

    Added Mode import / export

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import AirQuality from './lib/airQuality';
 import Disruptions from './lib/disruptions';
 import Line from './lib/line';
+import Mode from './lib/mode';
 import Road from './lib/road';
 import StopPoint from './lib/stopPoint';
 
-export { AirQuality, Disruptions, Line, Road, StopPoint };
+export { AirQuality, Disruptions, Line, Mode, Road, StopPoint };
