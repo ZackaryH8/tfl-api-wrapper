e89ac51baac3f24b018e29f42510796835694264
ZackaryH8 <zackary12389@yahoo.co.uk>
2021-01-20 19:11:33 +0000

    Added mode.ts Created a new 'Mode' class with getActiveServiceTypes and getArrivalPredictionsAllStops methods

---
+++ lib/mode.ts
@@ -0,0 +1,24 @@
+import TfLAPI from './tfl';
+import * as interfaces from './interfaces';
+
+export default class Mode extends TfLAPI {
+    constructor(config: interfaces.config) {
+        super(config);
+    }
+
+    /**
+     * Returns the service type active for a mode. Currently only supports tube
+     */
+    getActiveServiceTypes() {
+        return this.sendRequest(`/Mode/ActiveServiceTypes`, {}, 'GET');
+    }
+
+    /**
+     *
+     * @param mode A mode name e.g. tube, dlr
+     * @param count A number of arrivals to return for each stop, -1 to return all available.
+     */
+    getArrivalPredictionsAllStops(mode: string, count: number) {
+        return this.sendRequest(`/Mode/${mode}/Arrivals`, { count }, 'GET');
+    }
+}
