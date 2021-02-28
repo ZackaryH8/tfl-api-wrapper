f44d3ee8f1a3ca71572e4d3b7e6953b48098effa
ZackaryH8 <zackary12389@yahoo.co.uk>
2021-02-28 05:24:16 +0000

    Added TrackerNetLines enum

---
+++ lib/enums/lines.ts
@@ -0,0 +1,12 @@
+export enum TrackerNetLines {
+    Bakerloo = 'B',
+    Central = 'C',
+    District = 'D',
+    HammersmithCity = 'H',
+    Jubilee = 'J',
+    Metropolitan = 'M',
+    Northern = 'N',
+    Piccadilly = 'P',
+    Victoria = 'V',
+    WaterlooCity = 'W',
+}
