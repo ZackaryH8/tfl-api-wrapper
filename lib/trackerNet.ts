fdde04c28a523eeec003e5d6f2e76782d38c5f6a
ZackaryH8 <zackary12389@yahoo.co.uk>
2021-02-28 03:56:02 +0000

    Add getAllLinesStatus method Added getAllLinesStatus method and restructure the format of what is returned from XML conversion

--- lib/trackerNet.ts
+++ lib/trackerNet.ts
@@ -1,5 +1,6 @@
 import TfLAPI from './tfl';
 import * as interfaces from './interfaces';
+import { ITrackerNet } from './interfaces/trackerNet';
 
 export default class TrackerNet extends TfLAPI {
     constructor(config: interfaces.config) {
@@ -13,5 +14,31 @@ export default class TrackerNet extends TfLAPI {
      */
     getPredictionDetailed(line: string, stationCode: string): Promise<interfaces.ITrackerNet.RootObject> {
         return this.sendRequestTrackerNet(`/PredictionDetailed/${line}/${stationCode}`, {}, 'GET');
+    }
+
+    /**
+     * This service will return the status of all lines on the network indicating any delays, disruptions or suspensions on the lines.
+     * @param IncidentsOnly An indication of whether only lines that have incidents should be returned
+     */
+    async getAllLinesStatus(IncidentsOnly?: boolean): Promise<ITrackerNet.ArrayOfLineStatus> {
+        let uri = IncidentsOnly ? '/IncidentsOnly' : '';
+        const req = await this.sendRequestTrackerNet(`/LineStatus${uri}`, {}, 'GET');
+
+        return req.ArrayOfLineStatus.LineStatus.map((line: any) => {
+            return {
+                id: line.$.ID,
+                statusDetails: line.$.StatusDetails,
+                branchDisruptions: line.BranchDisruptions,
+                line: line.Line.map((x: any) => {
+                    return { id: x.$.ID, name: x.$.Name };
+                }),
+                status: line.Status.map((obj: any) => {
+                    return { id: obj.$.ID, cssClass: obj.$.CssClass, description: obj.$.Description, isActive: !!obj.$.IsActive };
+                }),
+                statusType: line.Status.map((obj: any) => {
+                    return { id: obj.$.ID, description: obj.$.Description };
+                }),
+            };
+        });
     }
 }
