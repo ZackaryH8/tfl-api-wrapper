54c119cf768a394af4f384089e3218e472406e39
ZackaryH8 <zackary12389@yahoo.co.uk>
2021-02-28 05:23:13 +0000

    Restructure return for getPredictionDetailed method getPredictionDetailed now returns a readable format

--- lib/trackerNet.ts
+++ lib/trackerNet.ts
@@ -9,11 +9,47 @@ export default class TrackerNet extends TfLAPI {
 
     /**
      * This service will return detailed train prediction information for a nominated station on a nominated line within 100 minute range.
-     * @param line A code representing the line (see Appendix A - {@link http://content.tfl.gov.uk/trackernet-data-services-guide-beta.pdf|TFL Docs})
-     * @param stationCode A code representing the station (see Appendix B - {@link http://content.tfl.gov.uk/trackernet-data-services-guide-beta.pdf|TFL Docs})
+     * @param line A code representing the line (Appendix A - {@link http://content.tfl.gov.uk/trackernet-data-services-guide-beta.pdf TFL Docs})
+     * @param stationCode A code representing the station (Appendix B - {@link http://content.tfl.gov.uk/trackernet-data-services-guide-beta.pdf TFL Docs})
      */
-    getPredictionDetailed(line: string, stationCode: string): Promise<interfaces.ITrackerNet.RootObject> {
-        return this.sendRequestTrackerNet(`/PredictionDetailed/${line}/${stationCode}`, {}, 'GET');
+    async getPredictionDetailed(line: string, stationCode: string): Promise<ITrackerNet.PredictionDetailed> {
+        const request = await this.sendRequestTrackerNet(`/PredictionDetailed/${line}/${stationCode}`, {}, 'GET');
+        const root = request.ROOT;
+        return {
+            information: {
+                code: root.S[0].$.Code,
+                name: root.S[0].$.N.slice(0, -1),
+                currentTime: root.S[0].$.CurTime,
+            },
+            platforms: root.S[0].P.map((platform: any) => {
+                return {
+                    name: platform.$.N,
+                    number: platform.$.Num,
+                    trackCode: platform.$.TrackCode,
+                    nextTrain: platform.$.NextTrain,
+                    trains: platform?.T?.map((train: any) => {
+                        return {
+                            trainID: train.$.TrainId,
+                            LCID: train.$.LCID,
+                            setNumber: train.$.SetNo,
+                            tripNumber: train.$.TripNo,
+                            timeToStation: train.$.TimeTo,
+                            currentLocation: train.$.Location,
+                            destination: train.$.Destination,
+                            destinationCode: train.$.DestCode,
+                            departedTime: train.$.DepartTime,
+                            departureInterval: train.$.DepartInterval,
+                            hasDeparted: !!train.$.Departed,
+                            direction: train.$.Direction,
+                            isStalled: train.$.IsStalled,
+                            trackCode: train.$.TrackCode,
+                            line: train.$.LN,
+                            leadingCarNumber: train.$.LeadingCarNo,
+                        };
+                    }),
+                };
+            }),
+        };
     }
 
     /**
@@ -27,11 +63,9 @@ export default class TrackerNet extends TfLAPI {
         return request.ArrayOfLineStatus.LineStatus.map((line: any) => {
             return {
                 id: line.$.ID,
+                name: line.Line[0].$.Name,
                 statusDetails: line.$.StatusDetails,
                 branchDisruptions: line.BranchDisruptions,
-                line: line.Line.map((x: any) => {
-                    return { id: x.$.ID, name: x.$.Name };
-                }),
                 status: line.Status.map((obj: any) => {
                     return { id: obj.$.ID, cssClass: obj.$.CssClass, description: obj.$.Description, isActive: !!obj.$.IsActive };
                 }),
