c8f0f3cf69e4b2a388ba7e046fdc1236f16b964e
ZackaryH8 <zackary12389@yahoo.co.uk>
2021-02-28 05:23:34 +0000

    Added PredictionDetailed interface

--- lib/interfaces/trackerNet.ts
+++ lib/interfaces/trackerNet.ts
@@ -16,6 +16,44 @@ export declare module ITrackerNet {
         description: string;
     }
 
+    export interface Information {
+        code: string;
+        name: string;
+        currentTime: string;
+    }
+
+    export interface Train {
+        trainID: string;
+        LCID: string;
+        setNumber: string;
+        tripNumber: string;
+        timeToStation: string;
+        currentLocation: string;
+        destination: string;
+        destinationCode: string;
+        departedTime: string;
+        departureInterval: string;
+        hasDeparted: boolean;
+        direction: string;
+        isStalled: string;
+        trackCode: string;
+        line: string;
+        leadingCarNumber: string;
+    }
+
+    export interface Platform {
+        name: string;
+        number: string;
+        trackCode: string;
+        nextTrain: string;
+        trains: Train[];
+    }
+
+    export interface PredictionDetailed {
+        information: Information;
+        platforms: Platform[];
+    }
+
     export interface ArrayOfLineStatus {
         id: string;
         statusDetails: string;
