2ccd7512f29d7593167d61ea5c910f95db653f90
ZackaryH8 <zackary12389@yahoo.co.uk>
2021-02-28 15:56:16 +0000

    Change interface handling and names

--- lib/interfaces/trackerNet.ts
+++ lib/interfaces/trackerNet.ts
@@ -1,23 +1,75 @@
-export declare module ITrackerNet {
+export declare module getPredictionSummary {
+    export interface Train {
+        /* The set number of the train */
+        setNumber: string;
+
+        /* The trip number of the train */
+        tripNumber: string;
+
+        /* A code representing the destination of the train */
+        destinationCode: string;
+
+        /* A value in representing the ‘time to station’ for this train in the format MM:SS */
+        timeToStation: string;
+
+        /* The current location of the train */
+        currentLocation: string;
+
+        /* The name of the destination of the train */
+        destination: string;
+    }
+
+    export interface Platform {
+        /* The name of the platform */
+        name: string;
+
+        /* A code representing the platform */
+        code: string;
+        next: string;
+
+        /* An array of trains in the prediction list */
+        trains: Train[];
+    }
+
+    /* An object representing a station on the line */
+    export interface Station {
+        /* A code representing the station */
+        code: string;
+
+        /* The name of the station */
+        name: string;
+
+        /* An array  of platforms on the station */
+        platforms: Platform[];
+    }
+
+    export interface Root {
+        /* The date/time the service was run in the format YYYY/MM/DD HH:MM:SS */
+        currentTime: string;
+        stations: Station[];
+    }
+}
+
+export declare module getPredictionDetailed {
     export interface Line {
-        /** A code representing the line */
+        /* A code representing the line */
         id: string;
 
-        /** The line name */
+        /* The line name */
         name: string;
     }
 
     export interface Status {
-        /** Status ID A numeric code representing the status of the line */
+        /* Status ID A numeric code representing the status of the line */
         id: string;
 
-        /** A text code representing the general status of the line, e.g. GoodService, DisruptedService */
+        /* A text code representing the general status of the line, e.g. GoodService, DisruptedService */
         cssClass: string;
 
-        /** A description of the status of the line e.g. Part Suspended, Severe Delays */
+        /* A description of the status of the line e.g. Part Suspended, Severe Delays */
         description: string;
 
-        /** A Boolean indicating if the status shown is active */
+        /* A Boolean indicating if the status shown is active */
         isActive: boolean;
     }
 
@@ -27,92 +79,92 @@ export declare module ITrackerNet {
     }
 
     export interface Information {
-        /** A code representing the station */
+        /* A code representing the station */
         code: string;
 
-        /** The name of the station */
+        /* The name of the station */
         name: string;
 
-        /** The time the service was run in the format HH:MM:SS */
+        /* The time the service was run in the format HH:MM:SS */
         currentTime: string;
     }
 
-    /** An object representing a train in the prediction list */
+    /* An object representing a train in the prediction list */
     export interface Train {
         trainID: string;
 
-        /** The leading car Id for the train */
+        /* The leading car Id for the train */
         LCID: string;
 
-        /** The set number of the train */
+        /* The set number of the train */
         setNumber: string;
 
-        /** The trip number of the train */
+        /* The trip number of the train */
         tripNumber: string;
 
-        /** A value representing the ‘time to station’ for this train in seconds in the format SSS */
+        /* A value representing the ‘time to station’ for this train in seconds in the format SSS */
         secondsToStation: string;
 
-        /** A value representing the ‘time to station’ for this train in minutes and seconds in the format MM:SS */
+        /* A value representing the ‘time to station’ for this train in minutes and seconds in the format MM:SS */
         timeTo: string;
 
-        /** The current location of the train */
+        /* The current location of the train */
         currentLocation: string;
 
-        /** The name of the destination of the train */
+        /* The name of the destination of the train */
         destination: string;
 
-        /** A code representing the destination of the train */
+        /* A code representing the destination of the train */
         destinationCode: string;
 
-        /** The time train departed the platform in the format `H:MM:SS` */
+        /* The time train departed the platform in the format `H:MM:SS` */
         departedTime: string;
 
-        /** Interval in seconds between the departure of the specified train and the previous train */
+        /* Interval in seconds between the departure of the specified train and the previous train */
         departureInterval: string;
 
-        /** Boolean value to determine if the train has departed the platform */
+        /* Boolean value to determine if the train has departed the platform */
         hasDeparted: boolean;
 
-        /** Direction of Travel */
+        /* Direction of Travel */
         direction: string;
 
-        /** The current section of track the train occupies */
+        /* The current section of track the train occupies */
         trackCode: string;
 
-        /** A code representing the line the train is running on */
+        /* A code representing the line the train is running on */
         line: string;
 
         leadingCarNumber: string;
     }
 
-    /** An object representing a platform on the station */
+    /* An object representing a platform on the station */
     export interface Platform {
         name: string;
         number: string;
 
-        /** The track code of the section of track at the front of the platform */
+        /* The track code of the section of track at the front of the platform */
         trackCode: string;
         nextTrain: string;
 
-        /** An array containing a Train object */
+        /* An array containing a Train object */
         trains?: Train[];
     }
 
-    /** This service will return detailed train prediction information for a nominated station on a nominated line within 100 minute range */
-    export interface PredictionDetailed {
-        /** An object containing basic station information */
+    /* This service will return detailed train prediction information for a nominated station on a nominated line within 100 minute range */
+    export interface Root {
+        /* An object containing basic station information */
         information: Information;
 
-        /** An array of platforms on the station */
+        /* An array of platforms on the station */
         platforms: Platform[];
     }
 
     export interface ArrayOfLineStatus {
-        /** An identifier for the line */
+        /* An identifier for the line */
         id: string;
 
-        /** A description of the status of the line if the status is not normal otherwise this will be blank */
+        /* A description of the status of the line if the status is not normal otherwise this will be blank */
         statusDetails: string;
         line: Line[];
         status: Status[];
