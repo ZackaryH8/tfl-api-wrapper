1c8c0b26a6b5420b7ef93fdac01630d646d6d8c5
Zack Eckersley Pallett <zackary12389@yahoo.co.uk>
2021-03-16 22:27:06 +0000

    Added modes enum

---
+++ lib/enums/modes.ts
@@ -0,0 +1,21 @@
+const enum modes {
+    bus = 'bus',
+    cablecar = 'cable-car',
+    cycle = 'cycle',
+    cyclehire = 'cycle-hire',
+    dlr = 'dlr',
+    interchangekeepsitting = 'interchange-keep-sitting',
+    interchangesecure = 'interchange-secure',
+    nationalrail = 'national-rail',
+    overground = 'overground',
+    replacementbus = 'replacement-bus',
+    riverbus = 'river-bus',
+    rivertour = 'river-tour',
+    taxi = 'taxi',
+    tflrail = 'tflrail',
+    tram = 'tram',
+    tube = 'tube',
+    walking = 'walking',
+}
+
+export default modes;
