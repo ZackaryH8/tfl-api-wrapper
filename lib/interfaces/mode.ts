64e0ea116bb47641034e8bb6171af9da4b8c10b1
ZackaryH8 <zackary12389@yahoo.co.uk>
2021-04-21 09:29:02 +0000

    Add interfaces for /Mode

---
+++ lib/interfaces/mode.ts
@@ -0,0 +1,42 @@
+export interface ActiveServiceTypes {
+    $type: string;
+    mode: string;
+    serviceType: string;
+}
+
+export declare module Arrivals {
+    export interface Timing {
+        $type: string;
+        countdownServerAdjustment: string;
+        source: Date;
+        insert: Date;
+        read: Date;
+        sent: Date;
+        received: Date;
+    }
+
+    export interface Root {
+        $type: string;
+        id: string;
+        operationType: number;
+        vehicleId: string;
+        naptanId: string;
+        stationName: string;
+        lineId: string;
+        lineName: string;
+        platformName: string;
+        direction: string;
+        bearing: string;
+        destinationNaptanId: string;
+        destinationName: string;
+        timestamp: Date;
+        timeToStation: number;
+        currentLocation: string;
+        towards: string;
+        expectedArrival: Date;
+        timeToLive: Date;
+        modeName: string;
+        timing: Timing;
+    }
+
+}
