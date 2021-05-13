c326e3ce4c32a34837c5c4c258b216a80e2edd73
ZackaryH8 <zackary12389@yahoo.co.uk>
2021-05-13 08:52:04 +0000

    Add types to bikepoint

--- lib/bikePoint.ts
+++ lib/bikePoint.ts
@@ -1,20 +1,21 @@
 import TfLAPI from './tfl';
 import Config from './interfaces/config';
+import { IBikePoint } from './interfaces/bikepoint';
 
 export default class BikePoint extends TfLAPI {
     constructor(config: Config) {
         super(config);
     }
 
-    getAll() {
+    getAll(): Promise<IBikePoint.Root[]> {
         return this.sendRequest(`/BikePoint/`, {}, 'GET');
     }
 
-    getByID(id: string) {
+    getByID(id: string): Promise<IBikePoint.Root> {
         return this.sendRequest(`/BikePoint/${id}`, {}, 'GET');
     }
 
-    getByName(query: string) {
+    getByName(query: string): Promise<IBikePoint.Root> {
         return this.sendRequest(`/BikePoint/Search`, { query }, 'GET');
     }
 }
