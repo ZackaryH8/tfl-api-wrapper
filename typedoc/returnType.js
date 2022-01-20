fe428f880a5ab84174e36b381e8692080116cdab
Zack <zackary12389@yahoo.co.uk>
2022-01-20 02:08:22 +0000

    Remove log

--- typedoc/returnType.js
+++ typedoc/returnType.js
@@ -2,7 +2,6 @@ const { Converter, ReflectionKind, UnknownType } = require('typedoc');
 
 exports.load = function (app) {
     app.converter.on(Converter.EVENT_CREATE_SIGNATURE, (ctx, sig) => {
-        console.log(sig);
         if (sig.kind === ReflectionKind.CallSignature) {
             sig.type = new UnknownType('');
         }
