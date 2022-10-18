4678cb86911b84c765a7e1c7081075c24ebb4898
Zack Eckersley Pallett <zackary12389@yahoo.co.uk>
2022-10-18 23:55:19 +0100

    Fix comment spelling

--- lib/tfl.ts
+++ lib/tfl.ts
@@ -83,7 +83,7 @@ export default class TfLAPI {
     }
 
     /**
-     * Checl for incidents only
+     * Check for incidents only
      * @param incidentsOnly Boolean to check
      * @ignore
      */
